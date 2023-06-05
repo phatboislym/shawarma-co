58dd7a9fa47dc18df93074e7e6ef0005e3400ba9
Adewole Conde <phatboislym@protonmail.com>
2023-06-05 01:07:28 +0100

    update: add login endpoint

--- auth_routes.py
+++ auth_routes.py
@@ -4,7 +4,13 @@ from fastapi import APIRouter
 auth_router = APIRouter()
 
 
-@auth_router.get("/auth")
+@auth_router.get("/auth/register")
 async def register() -> dict:
-    message: dict = {"message": "auth route"}
+    message: dict = {"message": "registraton endpoint"}
+    return message
+
+
+@auth_router.get("/auth/login")
+async def login() -> dict:
+    message: dict = {"message": "login endpoint"}
     return message
