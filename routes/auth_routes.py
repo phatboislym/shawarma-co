d8f435ca87ea0c715fda87026a93272bcb11a21a
Adewole Conde <phatboislym@protonmail.com>
2023-06-13 23:49:57 +0100

    fix:    import hashing functions from workzeug.security

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,10 +1,9 @@
-from bcrypt import checkpw, gensalt, hashpw
-from starlette.status import HTTP_201_CREATED
 from db import engine, Session
 from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
 from models.user import User
 from schemas.signup import SignUp
+from werkzeug.security import generate_password_hash
 
 auth_router = APIRouter()
 session = Session(bind=engine)
@@ -16,7 +15,7 @@ async def auth() -> dict:
     return message
 
 
-@auth_router.post("/auth/register", status_code=HTTP_201_CREATED)
+@auth_router.post("/auth/register", status_code=status.HTTP_201_CREATED)
 async def register(user: SignUp):
     db_email = session.query(User).filter(User.email == user.email).first()
     if db_email:
@@ -31,7 +30,7 @@ async def register(user: SignUp):
 
     new_user: User = User(email=user.email, is_active=user.is_active,
                           is_staff=user.is_staff, name=user.name,
-                          password=hashpw(user.password, gensalt()),
+                          password=generate_password_hash(user.password),
                           username=user.username)
 
     session.add(new_user)
