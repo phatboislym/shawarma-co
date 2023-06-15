71084d91cb58343c6c48e25f15596942a657e896
Adewole Conde <phatboislym@protonmail.com>
2023-06-15 20:14:03 +0100

    add: Settings class for JWT Auth

---
+++ schemas/settings.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from os import environ
+from pydantic import BaseModel
+
+
+# Load the environmental variables from the .env file
+load_dotenv('./.env.development.local')
+
+
+class Settings(BaseModel):
+    """ Settings class that extends BaseModel from Pydantic """
+    authjwt_secret_key = environ.get(
+        'DB_SECRET', '76fc0673e8ebe931438d67d1c6b99f2d6de50bf78ff29ebf9f9291cbf4e94648')
