9f47a42585964b5b4769d630d4a8b423fad6f0ae
Adewole Conde <phatboislym@protonmail.com>
2023-06-15 20:05:29 +0100

    update: @AuthJWT.load_config

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-"""
+""""
 module for FastAPI app
 """
 
 from fastapi import FastAPI
+from fastapi_another_jwt_auth import AuthJWT
 from routes.auth_routes import auth_router
 from routes.order_routes import order_router
 from routes.user_routes import user_router
-from models.items import Item
 from models.order import Order
 from models.user import User
+from schemas.settings import Settings
+
 
 app = FastAPI()
+
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+
 app.include_router(auth_router)
 app.include_router(order_router)
 app.include_router(user_router)
