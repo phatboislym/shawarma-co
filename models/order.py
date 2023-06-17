05d0c3f16a7ae05982373617578444066cd9221c
Adewole Conde <phatboislym@protonmail.com>
2023-06-17 18:30:10 +0100

    update: change default value for spicyness

--- models/order.py
+++ models/order.py
@@ -30,7 +30,7 @@ class Order(Base):
                   ('LARGE', 'large'), ('EXTRA-LARGE', 'extra-large'))
 
     SPICYNESS = (('NO-SPICE', 'no-spice'), ('MILD', 'mild'),
-                 ('ORIGINAL', 'original'),
+                 ('MEDIUM', 'medium'),
                  ('SPICY', 'spicy'),
                  ('EXTRA-SPICY', 'extra-spicy'))
 
@@ -40,7 +40,7 @@ class Order(Base):
     quantity = Column(Integer, nullable=False)
     status = Column(ChoiceType(choices=ORDER_STATUSES), default='PENDING')
     size = Column(ChoiceType(choices=WRAP_SIZES), default='MEDIUM')
-    spicyness = Column(ChoiceType(choices=SPICYNESS), default='ORIGINAL')
+    spicyness = Column(ChoiceType(choices=SPICYNESS), default='MEDIUM')
     user_id = Column(Integer, ForeignKey('users.id_'))
     user = relationship('User', back_populates='orders')
 
