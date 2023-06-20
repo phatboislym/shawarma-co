c75342a158dd6a8ea6acd9d690f3b2cad9dfb54d
Adewole Conde <phatboislym@protonmail.com>
2023-06-20 02:32:18 +0100

    add: delete order endpoint

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -100,22 +100,80 @@ async def update_order(order_id: int, order: OrderModel,
     current_user_id = Authorize.get_raw_jwt()['sub']
     db_user: User = session.query(User).filter(
         User.username == current_user_id).first()
-    order_user: User = session.query(User).filter(
-        User.id_ == order_id).first()
     db_order = session.query(Order).filter(Order.id_ == order_id).first()
+    order_user: User = session.query(User).filter(
+        User.id_ == db_order.user_id).first()
     if not db_order:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     is_order_user: bool = db_order.user_id == order_user.id_
     if db_user.is_staff or is_order_user:
-        if db_order.user_id == db_user.id_:
+        if order.quantity:
             db_order.quantity = order.quantity
-            db_order.size = order.size
-            db_order.spicyness = order.spicyness
-            session.commit()
+        if order.size:
+            db_order.size = order.size.upper()
+        if order.spicyness:
+            db_order.spicyness = order.spicyness.upper()
+        session.commit()
         response = {"id": db_order.id_, "quantity": db_order.quantity,
                     "size": db_order.size,
                     "status": db_order.status}
         return jsonable_encoder(response)
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='admin or user access required')
+
+
+@order_router.patch("/orders/order/update/{order_id}/",
+                    status_code=status.HTTP_202_ACCEPTED)
+async def update_order_status(order_id: int, order: OrderModel,
+                              Authorize: AuthJWT = Depends()):
+
+    try:
+        Authorize.jwt_required()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='invalid token')
+
+    current_user_id = Authorize.get_raw_jwt()['sub']
+    db_user: User = session.query(User).filter(
+        User.username == current_user_id).first()
+    if db_user.is_staff:
+        db_order: Order = session.query(Order).filter(
+            Order.id_ == order_id).first()
+        if order.status:
+            db_order.status = order.status
+            session.commit()
+            response: dict = {"status": db_order.status}
+            return jsonable_encoder(response)
+        if not db_order:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='admin access required')
+
+
+@order_router.delete("/orders/order/delete/{order_id}",
+                     status_code=status.HTTP_202_ACCEPTED)
+async def delete_order(order_id: int, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='invalid token')
+    db_order: Order = session.query(Order).filter(
+        Order.id_ == order_id).first()
+    if not db_order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    current_user_id = Authorize.get_raw_jwt()['sub']
+    db_user: User = session.query(User).filter(
+        User.username == current_user_id).first()
+    order_user: User = session.query(User).filter(
+        User.username == current_user_id).first()
+    is_order_user: bool = db_user.id_ == order_user.id_
+    if db_user.is_staff or is_order_user:
+        session.delete(db_order)
+        session.commit()
+        return db_order
     else:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail='admin or user access required')
