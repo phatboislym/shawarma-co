bf9285824ccedff8f1d6b2976331b3fff5049af6
Adewole Conde <phatboislym@protonmail.com>
2023-06-26 00:21:06 +0100

    update schemas with UUIDType ids

--- schemas/order.py
+++ schemas/order.py
@@ -1,25 +1,27 @@
 from pydantic import BaseModel
+from sqlalchemy_utils import UUIDType
 from typing import Optional
 
 
 class OrderModel(BaseModel):
     """ schema for order model """
-    id_: Optional[int]
+    id_: Optional[UUIDType]
     quantity: int
     status: Optional[str] = 'PENDING'
     size: Optional[str] = 'MEDIUM'
-    spicyness: Optional[str] = 'MEDIUM'
-    user_id: Optional[int]
+    spiciness: Optional[str] = 'MEDIUM'
+    user_id: Optional[UUIDType]
 
     class Config:
         orm_mode = True
+        arbitrary_types_allowed = True
         schema_extra = {
             'example': {
-                'id_': 12,
+                'id_': '9c267dc4-4cf8-405a-a91a-b6577292c230',
                 'quantity': 5,
                 'status': 'PENDING',
                 'size': 'MEDIUM',
-                'spicyness': 'MEDIUM',
-                'user_id': 2
+                'spiciness': 'MEDIUM',
+                'user_id': '3afdcf22-05e0-468d-90bd-72439971a92e'
             }
         }
--- schemas/signup.py
+++ schemas/signup.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
+from sqlalchemy_utils import UUIDType
 from typing import Optional
 
 
 class SignUp(BaseModel):
     """ schema for SignUp model """
 
-    id_: Optional[int]
+    id_: Optional[UUIDType]
     username: str
     email: str
     name: Optional[str]
@@ -15,8 +16,10 @@ class SignUp(BaseModel):
 
     class Config:
         orm_mode = True
+        arbitrary_types_allowed = True
         schema_extra = {
             'example': {
+                'id_': '3afdcf22-05e0-468d-90bd-72439971a92e',
                 'username': "panenka",
                 'email': 'dinktheball@example.com',
                 'name': 'Antonín Panenka',
