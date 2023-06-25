427ff5aea99717cf73bf0ad63a4ae780a4caa09e
Adewole Conde <phatboislym@protonmail.com>
2023-06-26 00:19:53 +0100

    update models with UUID id types

--- models/order.py
+++ models/order.py
@@ -2,24 +2,28 @@
 module for SQLAlchemy model `Order` for a database table named `orders`
 """
 
+from datetime import datetime
 from db import Base
-from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import Column, DateTime, ForeignKey, Integer
 from sqlalchemy.orm import relationship
-from sqlalchemy_utils import ChoiceType
+from sqlalchemy_utils import ChoiceType, UUIDType
+from uuid import uuid4
 
 
 class Order(Base):
     """
     class for SQLAlchemy model `Order`
     attributes:
-        id_, primary key: Column[int]
+        id_, primary key: Column[UUIDType]
         quantity: Column[int],
         size: Column[ChoiceType],
-        spicyness: Column[ChoiceType]
+        spiciness: Column[ChoiceType]
         status: Column[ChoiceType]
         time: Column[DateTime]
         user: relationship
-        user_id: Column[int] 
+        user_id: Column[UUIDType] 
+        created_at, a datetime object: Column[DateTime]
+        updated_at, a datetime object: Column[DateTime]
     """
     ORDER_STATUSES = (('PENDING', 'pending'), ('IN-TRANSIT', 'in-transit'),
                       ('DELIVERED', 'delivered'), ('CANCELED', 'canceled'))
@@ -27,20 +31,22 @@ class Order(Base):
     WRAP_SIZES = (('SMALL', 'small'), ('MEDIUM', 'medium'),
                   ('LARGE', 'large'), ('EXTRA-LARGE', 'extra-large'))
 
-    SPICYNESS = (('NO-SPICE', 'no-spice'), ('MILD', 'mild'),
+    SPICINESS = (('NO-SPICE', 'no-spice'), ('MILD', 'mild'),
                  ('MEDIUM', 'medium'),
                  ('SPICY', 'spicy'),
                  ('EXTRA-SPICY', 'extra-spicy'))
 
     __tablename__ = 'orders'
 
-    id_ = Column(Integer, primary_key=True)
+    id_ = Column(UUIDType(binary=False), primary_key=True, default=uuid4)
     quantity = Column(Integer, nullable=False)
     status = Column(ChoiceType(choices=ORDER_STATUSES), default='PENDING')
     size = Column(ChoiceType(choices=WRAP_SIZES), default='MEDIUM')
-    spicyness = Column(ChoiceType(choices=SPICYNESS), default='MEDIUM')
-    user_id = Column(Integer, ForeignKey('users.id_'))
+    spiciness = Column(ChoiceType(choices=SPICINESS), default='MEDIUM')
+    user_id = Column(UUIDType, ForeignKey('users.id_'))
     user = relationship('User', back_populates='orders')
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
 
     def __repr__(self) -> str:
         model: str = f'<Order {self.id_}>'
--- models/user.py
+++ models/user.py
@@ -4,35 +4,38 @@ module for SQLAlchemy model `User` for a database table named `users`
 
 from db import Base
 
-from sqlalchemy import Boolean, Column, Integer, String, Text, UUID
+from datetime import datetime
+from sqlalchemy import Boolean, Column, DateTime, String, Text
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils import UUIDType
+from uuid import uuid4
 
 
 class User(Base):
     """
     class for SQLAlchemy model `User`
     attributes:
-        id_, primary key: Column[int]
+        id_, primary key: Column[UUIDType]
         email, a non-nullable string: Column[str]
         is_active, a boolean: Column[bool]
         is_staff, a boolean: Column[bool]
         name, a non-nullable string: Column[str]
         password, a non-nullable string: Column[Text]
-        reset_token, UUID string: Column[UUID], optional
-        session_id, UUID string: Column[UUID], optional
+        created_at, a datetime object: Column[DateTime]
+        updated_at, a datetime object: Column[DateTime]
     """
     __tablename__ = 'users'
 
-    id_ = Column(Integer, primary_key=True)
+    id_ = Column(UUIDType(binary=False), primary_key=True, default=uuid4)
     email = Column(String(100), nullable=False, unique=True)
     is_active = Column(Boolean, default=False)
     is_staff = Column(Boolean, default=False)
     name = Column(String(100), nullable=True)
     orders = relationship('Order', back_populates='user')
     password = Column(Text, nullable=False)
-    reset_token = Column(UUID, nullable=True)
-    session_id = Column(UUID, nullable=True)
     username = Column(String(50), nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
 
     def __repr__(self) -> str:
         """ returns a string representation of the instance """
