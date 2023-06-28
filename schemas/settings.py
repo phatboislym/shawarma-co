18d81b37af99abd6c488741f309ee0cc1c574687
Adewole Conde <phatboislym@protonmail.com>
2023-06-28 11:11:51 +0100

    add: location for JWT

--- schemas/settings.py
+++ schemas/settings.py
@@ -11,5 +11,9 @@ class Settings(BaseModel):
     """ Settings class that extends BaseModel from Pydantic """
     authjwt_secret_key = environ.get(
         'DB_SECRET', '76fc0673e8ebe931438d67d1c6b99f2d6de50bf78ff29ebf9f9291cbf4e94648')
+    # Specify access token lifetime
     access_token_expire_minutes: int = 30
+    # Specify refresh token lifetime
     refresh_token_expire_days: int = 15
+    # Specify token location as headers and cookies
+    authjwt_token_location = ['headers', 'cookies']
